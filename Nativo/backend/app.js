e8886077efb8d26d8bf64201938e8a5921bb1b57
diego0096 <dfqz93@hotmail.com>
2020-06-16 19:34:20 +0000

    app program

---
+++ Nativo/backend/app.js
@@ -0,0 +1,29 @@
+'use strict'
+//Load library express
+var express = require('express');
+//Load library bodyParser
+var bodyParser = require('body-parser');
+//Load library express Routes and execute express
+var app = express();
+
+// Files.js Routes
+var activities_routes = require('./routes/activities');
+var users_routes = require('./routes/users');
+//Middlewares layer before controller routes funtions
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//Routes */*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+app.use('/api', activities_routes)
+app.use('/api', users_routes)
+//Export */*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+module.exports = app;
