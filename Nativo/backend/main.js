9f8807940fb6079fdcce892866a57125f65d69c0
diego0096 <dfqz93@hotmail.com>
2020-06-16 19:45:49 +0000

    main content

---
+++ Nativo/backend/main.js
@@ -0,0 +1,23 @@
+'use strict'
+//Load librery mongosee
+var mongoose = require('mongoose');
+var app = require('./app'); //Express routes
+var port = 3700;
+
+//Conection db
+mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/nativo_db',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log(`connection to database established`)
+
+        //Create server
+        app.listen(port, () => {
+            console.log("Server connected by URL: Localhost: 3700")
+        });
+
+    }).catch(err => console.log(err));
